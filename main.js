d82012b35b2987d013396744ad14b0f6308b3db8
raulgonzalez739 <fibonacci739@gmail.com>
2022-02-15 21:24:58 -0500

    leer e imprimir datos iniciales

---
+++ main.js
@@ -0,0 +1,5 @@
+let valor = `Raul`;
+let nombre = prompt('Ingrese dato:', )
+
+console.log(valor, nombre);
+console.log(`variable valor contiene el dato: ${valor} y la variable nombre tiene el dato: ${nombre} `);
