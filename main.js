085ff35fa7e00a16d7df000c9554b106924f25d4
raulgonzalez739 <fibonacci739@gmail.com>
2022-02-16 22:01:07 -0500

    Suma

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-let valor = `Raul`;
-let nombre = prompt('Ingrese dato:', )
+// let valor = 'Raul';
+// let nombre = prompt('Ingrese dato:', )
 
-console.log(valor, nombre);
-console.log(`variable valor contiene el dato: ${valor} y la variable nombre tiene el dato: ${nombre} `);
+// console.log(valor, nombre);
+// console.log(`Variable valor contiene el dato: ${valor}, y la variable nombre tiene el dato: ${nombre} `);
+// alert(`Variable valor contiene el dato: ${valor}, y la variable nombre tiene el dato: ${nombre} `);
+
+/*** Suma de numeros****/
+let dato1 = parseInt(prompt ('Ingrese el primer valor de la operación: '));
+alert(`El primer valor ingresado es: ${dato1}`);
+let dato2 = parseInt(prompt ('Ingrese el segundo valor de la operación: '));
+alert(`El primer valor ingresado es: ${dato2}`);
+suma = dato1 + dato2;
+alert(`El resultado de la operaciòn suma es: ${suma}`);
+
