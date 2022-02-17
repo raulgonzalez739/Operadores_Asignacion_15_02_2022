7333d95f0f4f2d39b0af43b986c7f8ae81b4cad6
raulgonzalez739 <fibonacci739@gmail.com>
2022-02-16 22:05:31 -0500

    Suma

--- main.js
+++ main.js
@@ -12,4 +12,3 @@ let dato2 = parseInt(prompt ('Ingrese el segundo valor de la operación: '));
 alert(`El primer valor ingresado es: ${dato2}`);
 suma = dato1 + dato2;
 alert(`El resultado de la operaciòn suma es: ${suma}`);
-
