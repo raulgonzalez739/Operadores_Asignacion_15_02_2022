74dfeb26c3d4816f3463788b1d7f0942f638fae3
raulgonzalez739 <fibonacci739@gmail.com>
2022-02-17 19:37:16 -0500

    Comentarios de operaciones

--- main.js
+++ main.js
@@ -5,18 +5,23 @@
 // console.log(`Variable valor contiene el dato: ${valor}, y la variable nombre tiene el dato: ${nombre} `);
 // alert(`Variable valor contiene el dato: ${valor}, y la variable nombre tiene el dato: ${nombre} `);
 
-/*** Suma de numeros****/
+/*** Ingreso de datos****/
 let dato1 = parseInt(prompt ('Ingrese el primer valor de la operación: '));
 alert(`El primer valor ingresado es: ${dato1}`);
 let dato2 = parseInt(prompt ('Ingrese el segundo valor de la operación: '));
 alert(`El primer valor ingresado es: ${dato2}`);
+/* Suma */
 // suma = dato1 + dato2;
 // alert(`El resultado de la operaciòn suma es: ${suma}`);
+/* Resta */
 // resta = dato1 - dato2;
 // alert(`El resultado de la operaciòn resta es: ${resta}`);
+/* División */
 // div = dato1/dato2;
 // alert(`EL resultado de la operación división es: ${div}`);
+/* Multiplicación */
 // mult = dato1*dato2;
 // alert(`El resultado de la operación multiplicación es: ${mult}`);
+/* Exponenciación */
 expon = dato1**dato2;
 alert(`El resultado de la operación exponenciación es: ${expon}`);
