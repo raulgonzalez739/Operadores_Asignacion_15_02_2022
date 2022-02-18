320b829f2473e78d5f2afb703195fd7b9ac26909
raulgonzalez739 <fibonacci739@gmail.com>
2022-02-17 19:19:41 -0500

    Divsión

--- main.js
+++ main.js
@@ -10,7 +10,9 @@ let dato1 = parseInt(prompt ('Ingrese el primer valor de la operación: '));
 alert(`El primer valor ingresado es: ${dato1}`);
 let dato2 = parseInt(prompt ('Ingrese el segundo valor de la operación: '));
 alert(`El primer valor ingresado es: ${dato2}`);
-suma = dato1 + dato2;
-alert(`El resultado de la operaciòn suma es: ${suma}`);
-resta = dato1 - dato2;
-alert(`El resultado de la operaciòn resta es: ${resta}`);
+// suma = dato1 + dato2;
+// alert(`El resultado de la operaciòn suma es: ${suma}`);
+// resta = dato1 - dato2;
+// alert(`El resultado de la operaciòn resta es: ${resta}`);
+div = dato1/dato2;
+alert(`EL resultado de la operación división es: ${div}`)
