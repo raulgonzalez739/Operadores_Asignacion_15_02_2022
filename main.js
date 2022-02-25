36c9ee9071f190a05afe364c944a59d8697018c1
raulgonzalez739 <fibonacci739@gmail.com>
2022-02-24 19:46:08 -0500

    Jerarquia de operaciones

--- main.js
+++ main.js
@@ -1,27 +1,10 @@
-// let valor = 'Raul';
-// let nombre = prompt('Ingrese dato:', )
+// Piramide operaciones
 
-// console.log(valor, nombre);
-// console.log(`Variable valor contiene el dato: ${valor}, y la variable nombre tiene el dato: ${nombre} `);
-// alert(`Variable valor contiene el dato: ${valor}, y la variable nombre tiene el dato: ${nombre} `);
-
-/*** Ingreso de datos****/
-let dato1 = parseInt(prompt ('Ingrese el primer valor de la operación: '));
-alert(`El primer valor ingresado es: ${dato1}`);
-let dato2 = parseInt(prompt ('Ingrese el segundo valor de la operación: '));
-alert(`El primer valor ingresado es: ${dato2}`);
-/* Suma */
-// suma = dato1 + dato2;
-// alert(`El resultado de la operaciòn suma es: ${suma}`);
-/* Resta */
-// resta = dato1 - dato2;
-// alert(`El resultado de la operaciòn resta es: ${resta}`);
-/* División */
-// div = dato1/dato2;
-// alert(`EL resultado de la operación división es: ${div}`);
-/* Multiplicación */
-// mult = dato1*dato2;
-// alert(`El resultado de la operación multiplicación es: ${mult}`);
-/* Exponenciación */
-expon = dato1**dato2;
-alert(`El resultado de la operación exponenciación es: ${expon}`);
+let resul  = 5*((4/4)*(9-7)*2)+6;
+//           5*(( 1 )*( 2 )*2)+6;
+//           5*(      4      )+6;
+//                   20       +6;
+//                          26     
+//
+//
+console.log(resul);
